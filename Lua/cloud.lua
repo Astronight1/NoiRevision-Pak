770275fa69b7878b6c4232962b2d21d44023c1fb
Astronight <johansanchez662007@hotmail.com>
2024-11-15 17:26:33 -0500

    as it turns out, the plan was actually so simple
    i was so mad when i found out that the cloud variables are exposed in lua for some reason, but this lets me completely override the clouds behavior so 99s

--- Lua/cloud.lua
+++ Lua/cloud.lua
@@ -2,86 +2,32 @@
 -- needed to be used for clouds little "hitlag" effect instead of
 -- actually just using hitlag needs a lil bompk on the head ngl
 
--- which means we're gonna havetuh unbullshit these clouds...
-local bullshit = false
-local unbullshit = false
-
-local driftstate = 0;
-local driftin = 0;
-
-local prevwavedash
-local prevwavedashboost
-local prevwavedashpower
-
-local multiplier
+-- which means we're gonna have tuh unbullshit these clouds...
+local CLOUD_ZTHRUST = 32*FRACUNIT
+local CLOUDB_ZTHRUST = 16*FRACUNIT
 
 local function cloudbounce(mobj, collider)
-    --if not (p.player) then return end
-    --local p = p.player
     local p = collider.player
     local playerspeed = R_PointToDist2(0, 0, p.mo.momx, p.mo.momy)
 
-    -- have to handle drifing manually
+    -- functionally disable normal cloud behaviour
+    p.cloud = 0
+    p.cloudlaunch = 0
+    p.cloudbuf = TICRATE
+    --
 
-    if p.cmd.turning > 0
-        multiplier = 1
-    elseif p.cmd.turning < 0
-        multiplier = -1
+    print(mobj.scale)
+
+    if mobj.type == MT_AHZ_CLOUD
+        p.mo.momz = FixedMul(mobj.scale, CLOUDB_ZTHRUST)
+    else
+        p.mo.momz = FixedMul(mobj.scale, CLOUD_ZTHRUST)
     end
 
-    if driftstate == 0
-        driftstate = 5 * multiplier
-    end
-
-    if unbullshit
-        p.wavedash = prevwavedash
-        p.wavedashboost = prevwavedashboost
-        p.wavedashpower = prevwavedashpower
-        
-        prevwavedash = 0
-        prevwavedashboost = 0
-        prevwavedashpower = 0
-        
-        if p.cmd.buttons&BT_DRIFT
-            p.driftcharge = driftin
-            --p.drift = driftstate
-            p.pflags = $|PF_DRIFTINPUT
-            p.pflags = $&~PF_DRIFTEND
-        end
-
-        driftstate = 0
-        driftin = 0
-
-        bullshit = false
-        unbullshit = false
-    end
-
-    if bullshit and playerspeed
-        unbullshit = true
-    end
-
-    if playerspeed <= 1*FRACUNIT
-        bullshit = true
-    end
-
-    if playerspeed > 1*FRACUNIT and not unbullshit
-        prevwavedash = p.wavedash
-        prevwavedashboost = p.wavedashboost
-        prevwavedashpower = p.wavedashpower
-        driftin = p.driftcharge
-        driftstate = p.drift
-    end
-
-    
-
-
-
-    p.drift = driftstate
-    --p.pflags = $&~PF_DRIFTEND
-
-    --print(p.drift)
-    --print(p.minspeed)
-    --print(driftstate)
-    --print("------")
+    p.fastfall = 0
 end
+
+-- because there's 3 versions of this shit for some reason
 addHook("TouchSpecial", cloudbounce, MT_AHZ_CLOUD)
+addHook("TouchSpecial", cloudbounce, MT_AGZ_CLOUD)
+addHook("TouchSpecial", cloudbounce, MT_SSZ_CLOUD)
